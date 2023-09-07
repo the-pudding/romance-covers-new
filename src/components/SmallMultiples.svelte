73abe101d0e7e34f3678dd28c5b16fc64406cbd2
Alice Liang <aliceyliang@gmail.com>
2023-09-07 15:18:46 -0400

    Mini charts inactive until hover

--- src/components/SmallMultiples.svelte
+++ src/components/SmallMultiples.svelte
@@ -13,25 +13,25 @@
 </script>
 
 <div id="small-multiples">
-    <div class="top-wrapper">
+    <div class="top-wrapper raunchiness">
         <h5>Raunchiness</h5>
-        <BarChart data={raunchinessData} color={"#7b9ef6"} highlightColor={"#4C7DFE"} pos={"inline"} yearTotals={yearTotals}/>
+        <BarChart data={raunchinessData} color={"#8eacf9"} highlightColor={"#4C7DFE"} pos={"inline"} yearTotals={yearTotals}/>
         <div class="year-labels">
             <p>2011</p>
             <p>2023</p>
         </div>
     </div>
-    <div class="top-wrapper">
+    <div class="top-wrapper illustration">
         <h5>Illustration</h5>
-        <BarChart data={illustrationData} color={"#fad872"} highlightColor={"#F7C42D"} pos={"inline"} yearTotals={yearTotals}/>
+        <BarChart data={illustrationData} color={"#fde59a"} highlightColor={"#F7C42D"} pos={"inline"} yearTotals={yearTotals}/>
         <div class="year-labels">
             <p>2011</p>
             <p>2023</p>
         </div>
     </div>
-    <div class="top-wrapper">
+    <div class="top-wrapper race">
         <h5>Racial Diversity</h5>
-        <BarChart data={raceData} color={"#50dada"} highlightColor={"#20B6B6"} pos={"inline"} yearTotals={yearTotals}/>
+        <BarChart data={raceData} color={"#7ce7e7"} highlightColor={"#20B6B6"} pos={"inline"} yearTotals={yearTotals}/>
         <div class="year-labels">
             <p>2011</p>
             <p>2023</p>
