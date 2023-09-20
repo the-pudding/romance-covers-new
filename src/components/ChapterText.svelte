6d41640313d08f0da69caf885cf3f381ed96b782
Jan Diehm <jadiehm@gmail.com>
2023-09-20 16:14:03 -0500

    update github pages

--- src/components/ChapterText.svelte
+++ src/components/ChapterText.svelte
@@ -4,14 +4,3 @@
 </script>
 
 <Prose copy={copy} />
-
-<style>
-    .prose {
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        max-width: 40rem;
-        margin: 0 auto;
-        line-height: 2;
-    }
-</style>
